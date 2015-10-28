36e9ead90e608056ffbdeb51421590623d412c26
RandAlThor1 <b.allen@28b5cs08.sjsd.lcl>
2015-10-28 10:47:28 -0500

    stats

---
+++ src/dungeonmaster/Stats.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeonmaster;
+
+/**
+ *
+ * @author b.allen
+ */
+public class Stats {
+    public int Strength;
+    public int Constitution;
+    public int Dexterity;
+    public int Intelligence;
+    public int Wisdom;
+    public int Charisma;
+    
+    public int StrengthMod;
+    public int ConstitutionMod;
+    public int DexterityMod;
+    public int IntelligenceMod;
+    public int WisdomMod;
+    public int CharismaMod;
+    
+    public int Health;
+    public int Speed;
+    public int DodgeChance;
+    
+    public String CharacterName;
+
+    public Stats(int Strength, int Constitution, int Dexterity, int Intelligence, int Wisdom, int Charisma, String CharacterName) {
+        this.Strength = Strength;
+        this.Constitution = Constitution;
+        this.Strength = Strength;
+        this.Dexterity = Dexterity;
+        this.Intelligence = Intelligence;
+        this.Wisdom = Wisdom;
+        
+        this.StrengthMod =      (int) ((Strength - 10) / 2);
+        this.ConstitutionMod =  (int) ((Constitution - 10) / 2);
+        this.DexterityMod =     (int) ((Dexterity - 10) / 2);
+        this.IntelligenceMod =  (int) ((Intelligence - 10) / 2);
+        this.WisdomMod =        (int) ((Wisdom - 10) / 2);
+        this.CharismaMod =      (int) ((Charisma - 10) / 2);
+        
+        this.Health = (int) 200 + ((Constitution - 10) / 2) * 50;
+        this.Speed = (int) 100 + ((Dexterity - 10) / 2) * 25;
+        this.DodgeChance = (int) 10 + ((Dexterity - 10) / 2) * 5;
+        
+        this.CharacterName = CharacterName;
+    }
+    
+    
+}
